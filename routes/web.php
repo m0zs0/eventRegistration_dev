36fae7f874e61660cf5d2df043486a228e2862dc
m0zs0 <monoki.zsolt@moriczref.hu>
2026-01-22 09:25:14 +0100

    web.php kész

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,31 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Admin\EventController;
+/*
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+*/
+
+Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function(){
+    Route::get('events',[EventController::class,'index'])->name('events.index');
+    Route::get('events/create',[EventController::class,'create'])->name('events.create');
+    Route::get('events',[EventController::class,'store'])->name('events.store');
+    Route::get('events/{event}/edit',[EventController::class,'edit'])->name('events.edit');
+    Route::get('events/{event}',[EventController::class,'update'])->name('events.update');
+    Route::get('events/{event}',[EventController::class,'destroy'])->name('events.destroy');
 });
